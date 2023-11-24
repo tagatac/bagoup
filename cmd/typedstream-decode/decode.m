80b45f8f1a7eda6665d7d1d07aabd8d248663af4
David Tagatac <david@tagatac.net>
2023-11-24 08:08:15 -0800

    Try building the objective-C program with GNUstep

--- cmd/typedstream-decode/decode.m
+++ cmd/typedstream-decode/decode.m
@@ -5,22 +5,15 @@
 //  typedstream-decode
 //
 
-#import "decode.h"
+#import <Foundation/Foundation.h>
 
-@implementation Decode
-
-- (void) decode:(NSString *) filename {
-    NSUnarchiver *typedStreamUnarchiver = [[NSUnarchiver alloc] initForReadingWithData:[NSData dataWithContentsOfFile:filename]];
-    id object = [typedStreamUnarchiver decodeObject];
-    printf("%s\n", [[NSString stringWithFormat:@"%@", object] UTF8String]);
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
+        NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
+        NSUnarchiver *typedStreamUnarchiver = [[NSUnarchiver alloc] initForReadingWithData:inputData];
+        id object = [typedStreamUnarchiver decodeObject];
+        printf("%s\n", [[NSString stringWithFormat:@"%@", object] UTF8String]);
+    }
+    return 0;
 }
-
-- (void) decodeStdin {
-    NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
-    NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
-    NSUnarchiver *typedStreamUnarchiver = [[NSUnarchiver alloc] initForReadingWithData:inputData];
-    id object = [typedStreamUnarchiver decodeObject];
-    printf("%s\n", [[NSString stringWithFormat:@"%@", object] UTF8String]);
-}
-
-@end
